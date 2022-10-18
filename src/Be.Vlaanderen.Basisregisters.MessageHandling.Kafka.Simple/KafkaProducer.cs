d2f18b323f2975acf4615e9396d7c3672f1f8917
Arne Dumarey <dumarey.arne@gmail.com>
2022-10-18 14:50:49 +0200

    feat: add Kafka Produce method without serialization

--- src/Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple/KafkaProducer.cs
+++ src/Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple/KafkaProducer.cs
@@ -10,6 +10,34 @@ namespace Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple
 
     public static class KafkaProducer
     {
+        public static async Task<Result> Produce(
+            KafkaProducerOptions options,
+            string key,
+            string message,
+            CancellationToken cancellationToken = default)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = options.BootstrapServers,
+                ClientId = Dns.GetHostName()
+            }.WithAuthentication(options);
+
+            try
+            {
+                await ProduceMessage(options, key, message, cancellationToken, config);
+
+                return Result.Success();
+            }
+            catch (ProduceException<Null, string> ex)
+            {
+                return Result.Failure(ex.Error.Code.ToString(), ex.Error.Reason);
+            }
+            catch (OperationCanceledException ex)
+            {
+                return Result.Failure(ex.Message, ex.Message);
+            }
+        }
+
         public static async Task<Result<T>> Produce<T>(
             KafkaProducerOptions options,
             string key,
@@ -28,12 +56,7 @@ namespace Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple
                 var serializer = JsonSerializer.CreateDefault(options.JsonSerializerSettings);
                 var kafkaJsonMessage = KafkaJsonMessage.Create(message, serializer);
 
-                using var producer = new ProducerBuilder<string, string>(config)
-                    .SetKeySerializer(Serializers.Utf8)
-                    .SetValueSerializer(Serializers.Utf8)
-                    .Build();
-
-                _ = await producer.ProduceAsync(new TopicPartition(options.Topic, new Partition(0)), new Message<string, string> { Key = key, Value = serializer.Serialize(kafkaJsonMessage) }, cancellationToken);
+                await ProduceMessage(options, key, serializer.Serialize(kafkaJsonMessage), cancellationToken, config);
                 return Result<T>.Success(message);
             }
             catch (ProduceException<Null, T> ex)
@@ -44,6 +67,18 @@ namespace Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Simple
             {
                 return Result<T>.Success(default);
             }
+        }
+
+        private static async Task ProduceMessage(KafkaProducerOptions options, string key, string message,
+            CancellationToken cancellationToken, ProducerConfig config)
+        {
+            using var producer = new ProducerBuilder<string, string>(config)
+                .SetKeySerializer(Serializers.Utf8)
+                .SetValueSerializer(Serializers.Utf8)
+                .Build();
+
+            _ = await producer.ProduceAsync(new TopicPartition(options.Topic, new Partition(0)),
+                    new Message<string, string> { Key = key, Value = message }, cancellationToken);
         }
     }
 }
